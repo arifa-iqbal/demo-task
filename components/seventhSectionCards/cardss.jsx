490dfc8c4505b2a2a04c1e43ba065e82fcf54d73
MEH-NAZZ <msmeh.naz07@gmail.com>
2024-06-01 16:49:42 +0500

    added few responsive things

--- components/seventhSectionCards/cardss.jsx
+++ components/seventhSectionCards/cardss.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Cards = (props) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-10 w-[100%] md:max-w-[305px] z-10 ">
-      <h2 className=" text-[28px] text-xl font-bold pb-2 mb-[8px] h-[70px] w-[204px] text-center ">{props.title}</h2>
-      <p className="text-gray-600 py-2 w-[244px] text-center ">{props.description}</p>
+    <div className="bg-white rounded-lg shadow-md p-10 w-[100%] md:max-w-[305px] z-10 text-center ">
+      <h2 className=" text-[28px] text-xl font-bold pb-2 mb-[8px] h-[70px] lg:w-[204px] w-[100%] text-center ">{props.title}</h2>
+      <p className="text-gray-600 py-2 lg:w-[244px] text-center w-[100%] ">{props.description}</p>
 
     </div>
   );
